45f69b2b8db179c51412e171b4715a0cdd9a3387
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-02-29 15:08:54 +0530

    referencing bucket name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_cloudtrail" "example" {
   name                          = "example"
-  s3_bucket_name                = aws_s3_bucket.example.id
+  s3_bucket_name                = "example"
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
